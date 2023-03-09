9e158d692ed7921570e8e754928a5c831255bd0c
ilybe <zaraharuein@gmail.com>
2023-03-09 13:25:09 -0500

    ok

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+def encode(password):
+    for num in password:
+        #turn password into int and add 3 then turn back into string
+        num = str(int(num) + 3)
+        password += num
+    print(password)
+
+def main():
+    while True:
+        print_menu()
+        option1 = int(input("Please enter an option:"))
+        if option1 == 1:
+            encode1 = input("Please enter your password to encode:")
+            encode(encode1)
+    #print menu using while loop
+    #ask for menu input
+    #ask for password
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
