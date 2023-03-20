49997aea59db59a73e860fce41edd2d8b7a84b88
ilybe <zaraharuein@gmail.com>
2023-03-20 17:46:22 -0400

    new

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ def print_menu():
 
 # hey
 def encode(password):
+    new_pass = ""
     for num in password:
         #turn password into int and add 3 then turn back into string
         num = str(int(num) + 3)
-        password += num
-    print(password)
+        new_pass += num
+    return(new_pass)
 
 def main():
     while True:
@@ -24,12 +25,11 @@ def main():
         option1 = int(input("Please enter an option:"))
         if option1 == 1:
             encode1 = input("Please enter your password to encode:")
-            encode(encode1)
+            print(encode(encode1))
     #print menu using while loop
     #ask for menu input
     #ask for password
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
