9bc8cafd23bc3066d0db5788fa669773d55cbd24
ilybe <zaraharuein@gmail.com>
2023-03-20 17:49:22 -0400

    new

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
+# Zara Haruna
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 def print_menu():
     print("Menu")
     print("-------------")
@@ -33,4 +32,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
